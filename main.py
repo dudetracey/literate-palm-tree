c5a13df791af6c9d954ac41a32bd3d27481d63d5
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-09-23 16:21:01 -0500

    Update main.py
    Brainstormed some ideas at top

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Is it possible to combine all my practice exercises into one repo on Github? This would be proof that I have completed all the exercises.
+# Maybe there is a way to make a menu to choose which exercise I want to show.
 # Write a program that prints out all the elements of a list that are less than 5.
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55 , 89]
 for num in a:
