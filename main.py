76b76b097cf783bc93f8629b08ad596140e45f5d
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-09-24 10:48:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Is it possible to combine all my practice exercises into one repo on Github? This would be proof that I have completed all the exercises.
 # Maybe there is a way to make a menu to choose which exercise I want to show.
+# The menu is probably best written as a function. Maybe I create all the different activities as functions and then I make a function that asks for which function.
+# Yes, that means a function of functions.
 # Write a program that prints out all the elements of a list that are less than 5.
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55 , 89]
 for num in a:
