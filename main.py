9362b2516d39c3c37552f780e8a18f1e6f37782b
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-09-25 09:23:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,24 @@
 # Maybe there is a way to make a menu to choose which exercise I want to show.
 # The menu is probably best written as a function. Maybe I create all the different activities as functions and then I make a function that asks for which function.
 # Yes, that means a function of functions.
+
+
+# Create a function that gives the first and last entries in a list
+import random
+
+
+def first_last():
+    return print(a[0], a[-1])
+
+
+a = random.sample(range(1000), 7)
+a = sorted(a)
+print(a)
+b = [first_last()]
+
+
+
+
 # Write a program that prints out all the elements of a list that are less than 5.
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55 , 89]
 for num in a:
