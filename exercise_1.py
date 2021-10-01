707779e54ac69236bb3dcc82f552801c315976ca
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-01 11:53:31 -0500

    Create exercise_1.py

---
+++ exercise_1.py
@@ -0,0 +1,6 @@
+# Exercise 1: Year to 100
+name = input('Give me your name: ')
+print('Your name is ' + name)
+age = input('Tell me your age: ')
+age = int(age)
+print(name + ', you will turn 100 in the year ' + str(2021 - age + 100))
