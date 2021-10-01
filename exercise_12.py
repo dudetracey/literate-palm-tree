a11ff64b0146dbbff5c89a7bbc9473ecd18d0e7b
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-01 12:25:04 -0500

    Create exercise_12.py

---
+++ exercise_12.py
@@ -0,0 +1,14 @@
+# Exercise 12
+# Create a function that creates a list with only the first and last entries of a longer list.
+
+import random
+
+
+def first_last():
+    return print(a[0], a[-1])
+
+
+a = random.sample(range(1000), 7)
+a = sorted(a)
+print(a)
+b = [first_last()]
