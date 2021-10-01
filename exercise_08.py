18e2eaf08de86fc3ca3667d779e086ce84f79825
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-01 12:05:56 -0500

    Create exercise_08.py
    One of my favorites!

---
+++ exercise_08.py
@@ -0,0 +1,30 @@
+# Exercise 8:
+# Two Player Rock-Paper-Scissors
+# Get input from the player, compare the inputs, and declare a winner. Ask if the players want to play again.
+
+player1 = input('Player 1 Name: ')
+player2 = input('Player 2 Name: ')
+quit = input('Do you want to play Rock, Paper Scissors? ')
+while quit != 'no':
+    if quit == 'no':
+        break
+    x = input((player1 + ', Rock, Paper, or Scissors? ')).lower()
+    y = input((player2 + ', Rock, Paper, or Scissors? ')).lower()
+    if x == y:
+        print('You are tied')
+    elif x == 'rock' and y == 'paper':
+        print(player2 + ' wins!!!')
+    elif x == 'rock' and y == 'scissors':
+        print(player1 + ' wins!!!')
+    elif x == 'paper' and y == 'scissors':
+        print(player2 + ' wins!!!')
+    elif x == 'paper' and y == 'rock':
+        print(player1 + ' wins!!!')
+    elif x == 'scissors' and y == 'rock':
+        print(player2 + ' wins!!!')
+    elif x == 'scissors' and y == 'paper':
+        print(player1 + ' wins!!!')
+    else:
+        print('Please type a correct choice for the game.')
+    quit = input('Do you want to play again? ').lower()
+print('Have an amazing day! ')
