cce593b6fc3655cc8274056ad82eb6dfa056fa6f
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-03 20:42:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@
 # Maybe there is a way to make a menu to choose which exercise I want to show.
 # The menu is probably best written as a function. Maybe I create all the different activities as functions and then I make a function that asks for which function.
 # Yes, that means a function of functions.
+
+from exercise_16 import password_generator
+password_generator()
