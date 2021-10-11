af4cce7c9f3aeab0870929a5b0df844696387aa0
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-11 15:36:24 -0500

    Update exercise_01.py

--- exercise_01.py
+++ exercise_01.py
@@ -1,6 +1,8 @@
 # Exercise 1: Year to 100
-name = input('Give me your name: ')
-print('Your name is ' + name)
-age = input('Tell me your age: ')
-age = int(age)
-print(name + ', you will turn 100 in the year ' + str(2021 - age + 100))
+def age_to_100():
+  name = input('Give me your name: ')
+  print('Your name is ' + name)
+  age = input('Tell me your age: ')
+  age = int(age)
+  print(name + ', you will turn 100 in the year ' + str(2021 - age + 100))
+  return
