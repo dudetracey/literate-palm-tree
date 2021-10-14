1c024bd38d075ee26166afebf5caf2e0d04e4cb5
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-13 21:20:18 -0500

    Update exercise_03.py

--- exercise_03.py
+++ exercise_03.py
@@ -1,10 +1,12 @@
 # Exercise 3 
 # Write a program that prints out all the elements of a list that are less than 5.
 # c variable is for checking the elements less than the user's input number.
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = []
-c = int(input('Give me a number to check: '))
-for num in a:
-    if num < c:
-        b.append(num)
-print(b)
+def less_than_5():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = []
+    c = int(input('Give me a number to check: '))
+    for num in a:
+        if num < c:
+            b.append(num)
+    print(b)
+return
