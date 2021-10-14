75e552060bf38ed91a6dab745bc5f174ce25e263
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-14 13:44:41 -0500

    Update exercise_04.py

--- exercise_04.py
+++ exercise_04.py
@@ -1,9 +1,11 @@
 # Exercise 4
 # Goal: User gives a number, the program returns all divisors of that number.
-a = int(input('Give me a number: '))
-b = range(2, a+1)
-c = []
-for num in b:
-    if a % num == 0:
-        c.append(num)
-print(c)
+def divisors():
+    a = int(input('Give me a number: '))
+    b = range(2, a+1)
+    c = []
+    for num in b:
+        if a % num == 0:
+            c.append(num)
+    print(c)
+return
