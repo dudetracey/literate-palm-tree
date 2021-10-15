7243dbb8fcc143c8aa4e86a0c73080a88730fcb3
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-15 09:47:18 -0500

    Update exercise_05.py

--- exercise_05.py
+++ exercise_05.py
@@ -1,10 +1,11 @@
 # Exercise 5:
 # Goal: given two lists, write a program that returns a list of the numbers that are included in both lists
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-c = []
-for num in a:
-    if num not in c:
-        if num in b:
-            c.append(num)
-print (c)
+def included():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    c = []
+    for num in a:
+        if num not in c:
+            if num in b:
+                c.append(num)
+    print (c)
