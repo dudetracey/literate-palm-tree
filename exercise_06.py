528000382be50cecb71ecab2f96f0e0934967466
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-16 09:30:54 -0500

    Update exercise_06.py

--- exercise_06.py
+++ exercise_06.py
@@ -1,15 +1,16 @@
 # Exercise 6:
 # Ask the user for a string, print out whether the string is a palindrome or not.
-a = str(input('Give me a phrase: '))
-b = a[::-1]
-while len(a) > 2:
-    if a.lower() == b.lower():
-        print('Yes, this is a palindrome')
+def palindrome():
+    a = str(input('Give me a phrase: '))
+    b = a[::-1]
+    while len(a) > 2:
+        if a.lower() == b.lower():
+            print('Yes, this is a palindrome')
+        else:
+            print('No, this is not a palindrome')
+        break
     else:
-        print('No, this is not a palindrome')
-    break
-else:
-    print('Please enter at least 2 characters.')
+        print('Please enter at least 2 characters.')
 
 # So the key insight here was how to reverse the original string. The secret is to [::-1]
 # I was actually really close but messed up the syntax. Make sure to do two : symbols
