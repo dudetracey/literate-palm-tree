2a17bdeeda97f5cd6406810a4e10950b58a8c88a
Dude Tracey <91097010+dudetracey@users.noreply.github.com>
2021-10-19 10:59:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This page could just import the exercises and then present the menu of possible choices.
 # After selecting an exercise, import that file and run it. 
 # I would need to add a description to either the main file of what the exercise is or at the top of the file for the exercise itself.
-
+import random
 from exercise_16 import password_generator
 password_generator()
 
